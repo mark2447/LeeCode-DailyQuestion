fec06ce552410ded0217d3f2e0c0128ed86e4fae
Mark Jay <hwj97055@gmail.com>
2024-03-04 21:54:56 +0800

    feat:并查集

--- java/src/day15-stock-trade/UnionFind.java
+++ java/src/day15-stock-trade/UnionFind.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class UnionFind {
+public class UnionFind {
+    //并查集
+    public int[] father;
+
+    public void init() {
+        for (int i = 0; i < father.length; i++) {
+            father[i] = i;
+        }
+    }
+
+    public int find(int n) {
+        if (father[n] == n) {
+            return n;
+        } else {
+            father[n] = find(father[n]);
+            return father[n];
+        }
+    }
+
+    public void join (int u, int v) {
+        int fu = father[u];
+        int fv = father[v];
+        if (fu == fv) {
+            return;
+        }
+        father[v] = u;
+
+
+
+
+
+    }
+
+    public boolean isSame(int u, int v) {
+        int fu = father[u];
+        int fv = father[v];
+        return fu == fv;
+    }
+
+
 }
